560876363d2576d54697699b5dc5bff106248d42
Bryan Maher <bm3n@andrew.cmu.edu>
2024-04-11 16:26:52 -0400

    Team Page Updates

---
+++ patterns/team-three-columns.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Title: Team with image, text, link.
+ * Slug: robby/team-three-columns
+ * Categories: featured
+ */
+?>
+<!-- wp:template-part {"slug":"header","theme":"robby","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","layout":{"type":"constrained","contentSize":"640px","wideSize":"1200px"}} -->
+<main class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+    <h2 class="wp-block-heading has-text-align-center">Meet the Team</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:separator -->
+    <hr class="wp-block-separator has-alpha-channel-opacity"/>
+    <!-- /wp:separator -->
+    
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column --></div>
+    <!-- /wp:columns --></div>
+    <!-- /wp:group -->
+    
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Student Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column --></div>
+    <!-- /wp:columns --></div>
+    <!-- /wp:group -->
+    
+    <!-- wp:heading {"textAlign":"center"} -->
+    <h2 class="wp-block-heading has-text-align-center">Faculty Advisors</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:separator -->
+    <hr class="wp-block-separator has-alpha-channel-opacity"/>
+    <!-- /wp:separator -->
+    
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Advisor Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:image {"id":109,"width":"200px","height":"200px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://student-project-website.local/wp-content/uploads/2024/04/robby-portrait.png" alt="" class="wp-image-109" style="width:200px;height:200px"/></figure>
+    <!-- /wp:image -->
+    
+    <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+    <h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1">Advisor Name</h3>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+    <p class="has-text-align-center" style="line-height:1.5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">View Profile</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:column --></div>
+    <!-- /wp:columns --></div>
+    <!-- /wp:group --></main>
+    <!-- /wp:group -->
+    
+    <!-- wp:template-part {"slug":"footer","theme":"robby","tagName":"footer","className":"site-footer"} /-->
