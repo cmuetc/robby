d9b244bff057783e56923a4fb653531e547e6201
Bryan Maher <bm3n@andrew.cmu.edu>
2024-04-15 14:50:56 -0400

    Added page-media pattern

---
+++ patterns/page-media.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Sample media page
+ * Slug: robby/page-media
+ * Categories: robby-page
+ */
+?>
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Requirements</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Replace <strong>all </strong>content on this page with <strong>your </strong>content.  Add or edit headings as needed, and <strong>delete </strong>unused placeholder content.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Print materials (Poster and Half Sheet) may be included along with videos.&nbsp; A short video (Elevator pitch) that contains a summary of the project (30 seconds) is required.&nbsp; At Weeks 12 – 15, replace it with a playable Final video (2 to 3 minutes) that documents the project in greater detail including a summary of the project semester and video documentation of the process.&nbsp; Still photos, additional videos that show gameplay, and playtesting, Halves presentations, and other informative visuals to your website are recommended.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Project Video</h2>
+<!-- /wp:heading -->
+
+<!-- wp:video -->
+<figure class="wp-block-video"><video controls poster="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/video-poster.png'; ?>" src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/videos/forbidden-planet-trailer.mp4'; ?>"></video></figure>
+<!-- /wp:video -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Half Sheet</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":204,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/robby-hafsheet.png'; ?>" alt="Project Halfsheet" class="wp-image-204"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Additional content...</h2>
+<!-- /wp:heading -->
